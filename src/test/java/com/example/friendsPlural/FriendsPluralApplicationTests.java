82424740365c40979f5013f79b4e4943773cb090
Shehroaz <muhammadshehroaz@gmail.com>
2021-04-28 01:13:22 +0500

    v 1.4 testing Smoke and Sanity test included.

--- src/test/java/com/example/friendsPlural/FriendsPluralApplicationTests.java
+++ src/test/java/com/example/friendsPlural/FriendsPluralApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.friendsPlural;
 
+import com.example.friendsPlural.controller.FriendController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class FriendsPluralApplicationTests {
+	@Autowired
+	FriendController friendController;
 
 	@Test
 	void contextLoads() {
+		Assert.notNull(friendController);
 	}
 
 }
